5d259758805cb4e7de865a4089c57d37b02b980e
Sasha <sashamiliatin@gmail.com>
2022-08-10 22:48:37 +0300

    Initial Commit6

--- app/main.py
+++ app/main.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
+import pandas as pd
+
+import handle_request
+import json
  
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
-        return "<h1> Hi Hen Hen/h1>"
+        return "<h1> Hi Hen Hen</h1>"
+
+#Create the receiver API POST endpoint:
+@app.route("/responer", methods=["POST","GET"])
+def postME():
+   request_data = request.get_json()
+   print("data recived: " + str(request_data))
+
+   game_name = request_data["user input"]["game_name"].lower()
+   data = pd.read_csv("machine_learning\df\clean_df_only_reviews.csv", encoding='utf-8')
+   gameNames = data['Game Title'].to_list()
+   gameNamesLowerCase = [x.lower() for x in gameNames]
+   res = any(game_name in string for string in gameNamesLowerCase)
+   if(res):
+      res_data = handle_request.response(request_data) #method that returns the response json
+   else:
+      res_data = -1
+
+
+   # res_data = handle_request.response(request_data) #method that returns the response json
+
+   response = app.response_class(
+      response=json.dumps(res_data),
+      status=200,
+      mimetype='application/json'
+   )
+
+   return response
+
