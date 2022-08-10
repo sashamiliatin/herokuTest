cc99e649d12049426c0af26ead83e55f1b811226
Sasha <sashamiliatin@gmail.com>
2022-08-10 23:19:56 +0300

    Initial Commit11

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def postME():
 
    game_name = request_data["user input"]["game_name"].lower()
    data = pd.read_csv("app/df/metacritic_critic_reviews.csv", encoding='utf-8')
-   gameNames = data['Game Title'].to_list()
+   gameNames = data['name'].to_list()
    gameNamesLowerCase = [x.lower() for x in gameNames]
    res = any(game_name in string for string in gameNamesLowerCase)
    if(res):
